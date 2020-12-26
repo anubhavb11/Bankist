0f5c778bce4123d38985254ae7a5f1f627ad62e7
anubhavb11 <43958843+anubhavb11@users.noreply.github.com>
2020-12-27 00:35:15 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,96 +1,304 @@
 'use strict';
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
+///////////////////////////////////////
+// Modal window
 
-// Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
 
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-const accounts = [account1, account2, account3, account4];
-
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
-
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
-
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
-
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-/////////////////////////////////////////////////
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-///for off loop
-
-for (const movement of movements) {
-  if (movement > 0) {
-    console.log(`Credited ${movement}`);
-  } else {
-    console.log(`debited ${Math.abs(movement)}`);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
   }
-}
+});
 
-console.log(`-------------for each---------------`);
+const btnScrollto = document.querySelector('.btn--scroll-to');
 
-movements.forEach(function (movement) {
-  if (movement > 0) {
-    console.log(`Credited ${movement}`);
-  } else {
-    console.log(`debited ${Math.abs(movement)}`);
+const section1 = document.querySelector('#section--1');
+
+btnScrollto.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+  // console.log(e);
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behaviour: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+////////////////Scrolling smooth //////////////////
+
+///////OLD method/////////////
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   console.log(el);
+
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = el.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+///////using eevnt di=elegation power of bubble in eevnrt propagation///////////////
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
+});
+
+// console.log(document.querySelectorAll('.operations__tab'));
+
+document
+  .querySelector('.operations__tab-container')
+  .addEventListener('click', function (e) {
+    const collect = e.currentTarget.children;
+    for (let i = 0; i < collect.length; i++) {
+      // console.log(collect[i]);
+      if (collect[i].classList.contains('operations__tab--active')) {
+        collect[i].classList.remove('operations__tab--active');
+      }
+    }
+    if (e.target.classList.contains('operations__tab')) {
+      e.target.classList.add('operations__tab--active');
+      const content = document.querySelectorAll('.operations__content ');
+      // console.log(content);
+
+      // we can use data set given in html to make code DRY
+      content.forEach(function (i) {
+        // console.log(i);
+        i.classList.remove('operations__content--active');
+
+        if (
+          e.target.classList.contains('operations__tab--1') &&
+          i.classList.contains('operations__content--1')
+        ) {
+          // console.log(i);
+          i.classList.add('operations__content--active');
+        }
+
+        if (
+          e.target.classList.contains('operations__tab--2') &&
+          i.classList.contains('operations__content--2')
+        ) {
+          // console.log(i);
+          i.classList.add('operations__content--active');
+        }
+
+        if (
+          e.target.classList.contains('operations__tab--3') &&
+          i.classList.contains('operations__content--3')
+        ) {
+          // console.log(i);
+          i.classList.add('operations__content--active');
+        }
+      });
+    }
+
+    //.forEach(function (i) {
+    //   console.log(i);
+    // });
+  });
+
+const nav = document.querySelector('.nav');
+
+const fn = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    const sibling = e.target.closest('.nav').querySelectorAll('.nav__link');
+
+    const logo = e.target.closest('.nav').querySelector('img');
+
+    sibling.forEach(function (el) {
+      if (el !== e.target) {
+        el.style.opacity = opacity;
+      }
+    });
+
+    logo.style.opacity = opacity;
+  }
+};
+
+nav.addEventListener('mouseover', function (e) {
+  // console.log(e.target);
+  fn(e, 0.5);
+});
+nav.addEventListener('mouseout', function (e) {
+  fn(e, 1);
+});
+
+// const scrollpos = section1.getBoundingClientRect();
+// window.addEventListener('scroll', function () {
+//   if (window.scrollY > scrollpos.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// });
+
+const obsCallback = function (entries, observer) {
+  entries.forEach(entry => {
+    // console.log(entry);
+    // console.log(entry.isIntersecting);
+    if (!entry.isIntersecting) {
+      // console.log('dsa');
+      nav.classList.add('sticky');
+    } else {
+      // console.log('wew');
+      nav.classList.remove('sticky');
+    }
+
+    // console.log(x.isInsersecting);
+  });
+  // const [x] = entries;
+  // console.log(x);
+  // console.log(x.isInsersecting);
+};
+
+const obsOptions = {
+  root: null,
+  threshold: [0, 1],
+  rootMargin: '-90px',
+};
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+
+const header = document.querySelector('.header');
+
+observer.observe(header);
+
+////////////////////////////////SLIDER////////////////////////////////
+
+const slider = document.querySelector('.slider');
+
+const slide = document.querySelectorAll('.slide');
+
+const slideLeft = document.querySelector('.slider__btn--left');
+const slideRight = document.querySelector('.slider__btn--right');
+
+const maxlenslide = slide.length;
+// console.log(slider);
+// slider.style.transform = 'scale(0.4) translateX(-120px)';
+
+// slider.style.overflow = 'visible';
+
+// console.log(slide);
+const dotContainer = document.querySelector('.dots');
+const createDots = function () {
+  slide.forEach(function (s, i) {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+
+createDots();
+
+const activeDot = function (slide) {
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+  document
+    .querySelector(`.dots__dot[data-slide ="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+
+slide.forEach(function (s, i) {
+  s.style.transform = `translateX(${i * 100}%)`;
+  // console.log(s);
+});
+activeDot(0);
+
+let currslide = 0;
+slideRight.addEventListener('click', function () {
+  if (currslide === maxlenslide - 1) {
+    currslide = 0;
+  } else {
+    currslide++;
+  }
+  slide.forEach(function (s, i) {
+    s.style.transform = `translateX(${100 * (i - currslide)}%)`;
+    // console.log(s);
+  });
+  activeDot(currslide);
+});
+
+slideLeft.addEventListener('click', function () {
+  if (currslide < 1) {
+    currslide = maxlenslide - 1;
+  } else {
+    currslide--;
+  }
+  slide.forEach(function (s, i) {
+    s.style.transform = `translateX(${100 * (i - currslide)}%)`;
+    // console.log(s);
+  });
+  activeDot(currslide);
+});
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e);
+  if (e.key === 'ArrowRight') {
+    // console.log('ds');
+    // slideRight.addEventListener('click', function () {
+    if (currslide === maxlenslide - 1) {
+      currslide = 0;
+    } else {
+      currslide++;
+    }
+    slide.forEach(function (s, i) {
+      s.style.transform = `translateX(${100 * (i - currslide)}%)`;
+      // console.log(s);
+    });
+    activeDot(currslide);
+  }
+
+  if (e.key === 'ArrowLeft') {
+    if (currslide < 1) {
+      currslide = maxlenslide - 1;
+    } else {
+      currslide--;
+    }
+    slide.forEach(function (s, i) {
+      s.style.transform = `translateX(${100 * (i - currslide)}%)`;
+      // console.log(s);
+    });
+    activeDot(currslide);
+  }
+});
+
+dotContainer.addEventListener('click', function (e) {
+  const pos = e.target.dataset.slide;
+  // console.log(e.target.dataset.slide);
+  console.log(pos);
+  slide.forEach(function (s, i) {
+    s.style.transform = `translateX(${100 * (i - pos)}%)`;
+    // console.log(s);
+  });
+  activeDot(pos);
 });
